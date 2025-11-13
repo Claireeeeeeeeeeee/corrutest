a23078bd881d1e10426f82c2daa936caf0b4fa81
Claire <claireeeeeeeeeeee@pm.me>
2025-11-13 04:57:41 -0500

    Update combatanimspeedtest.js
    optimal fix

--- combatanimspeedtest.js
+++ combatanimspeedtest.js
@@ -1,10 +1,12 @@
 //TODO: Add a dialogue with moth to change this..?
 
-//100% advance rate
-//env.ADVANCE_RATE = 1000
-
-//50% advance rate
-env.ADVANCE_RATE = 500
-
-//10% advance rate
-//env.ADVANCE_RATE = 100
+//This works wayyy better
+//sets it for all objects which might be a mistake but
+//ADVANCE_RATE is only used for combat stuff so
+//but for example this means there is 0 delay before entering a bullet hell segment
+//which might be fine
+Object.defineProperty(env, "ADVANCE_RATE", {
+  get: () => 0,
+  set: () => {}, // ignore any attempts to change it
+  configurable: true
+});
