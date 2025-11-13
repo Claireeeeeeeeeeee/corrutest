ae60a0a50e798d081d25a2475d39511dd280cb3e
Claire <claireeeeeeeeeeee@pm.me>
2025-11-13 04:02:50 -0500

    Update combatanimspeedtest.js
    Uncommented env.ADVANCE_RATE variable set and added TODO

--- combatanimspeedtest.js
+++ combatanimspeedtest.js
@@ -1,7 +1,10 @@
+//TODO: Add a dialogue with moth to change this..?
+
+//100% advance rate
+//env.ADVANCE_RATE = 1000
 
 //50% advance rate
-//env.ADVANCE_RATE = 500
+env.ADVANCE_RATE = 500
 
 //10% advance rate
-
 //env.ADVANCE_RATE = 100
